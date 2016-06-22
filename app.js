d08a4366df4b45a1ef3b86169cb394d740d98f9c
Moustachi <durablehotpocket@gmail.com>
2016-06-21 20:45:31 -0700

    AJAX call works

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-$.ajax({
-	type: "GET",
-	url: "https://www.reddit.com/r/funny",
-	dataType: "jsonp",
-	success: function(response) {
-		console.log(response);
-	}
+
+
+$(function(){
+
+	$("button").click(function(){
+		$.getJSON(
+        "https://www.reddit.com/search.json?q=cats",
+        function foo(data)
+        	{
+          		$.each(
+            	data.data.children.slice(0, 5),
+            	function (i, post) {
+             	 $(".result-box").append( '<br>' + post.data.title );
+             	 $(".result-box").append( '<br>' + post.data.url );
+             	 $(".result-box").append( '<br>' + post.data.permalink );
+             	 $(".result-box").append( '<hr>' );
+            	})
+            }	
+        )
+    });
 });
