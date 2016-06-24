f66cd93d0ac9f3d674cf53188441b42e2d500ffe
Moustachi <durablehotpocket@gmail.com>
2016-06-23 18:26:32 -0700

    added auto focus and enter key submit function

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-
-
-$(function(){
-
-	$("button").click(function(){
-		var searchTerm = $("input").val();
-		$.getJSON(
-        "https://www.reddit.com/search.json?q="+searchTerm+"",
-        function foo(data)
-        	{
-          		$.each(
-            	data.data.children.slice(0, 5),
-            	function (i, post) {
-             	 $(".result-box").append( '<br>' + post.data.title );
-             	 $(".result-box").append( '<br>' + post.data.url );
-             	 $(".result-box").append( '<br>' + post.data.permalink );
-             	 $(".result-box").append( '<hr>' );
-            	})
-            }	
-        )
-    });
+$("input").keypress(function (e) {
+  if (e.which == 13) {
+    e.preventDefault();
+    clearResults();
+    makeCall();
+  	}
 });
+
+function makeCall() {
+    var searchTerm = $("input").val();
+    $.getJSON(
+        "https://www.reddit.com/search.json?q=" + searchTerm + "",
+        function foo(data) {
+            $.each(
+                data.data.children.slice(0, 5),
+                function(i, post) {
+                    $(".result-box").append(post);
+                    $(".result-box").append("<a href='"+ post.data.url +
+                    "' target='_blank'>" + post.data.title + "</a>");
+                    $(".result-box").append('<br>' + post.data.permalink);
+                    $(".result-box").append('<hr>');
+                })
+        }
+    )
+};
+
+function clearResults() {
+	$(".result-box").empty();
+};
+
+$(function() {
+    $("button").click(function() {
+    	clearResults();
+        makeCall();
+    });
+
+});
+
+/*
+	var post =
+            	 "<div class='post-item'>" +
+            	 	"<h3>" + post.data.title + "</h3>" +
+            	 	"<img src='"+ post.data.url + "'>" +
+            	 	"<a href='https://reddit.com" + post.data.permalink + "'> Reddit Link <a>" +
+            	 	"<hr>" +
+            	 "</div>"
+*/
